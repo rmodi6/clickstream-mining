0d6df95d2359bf27ed607fb4b418957fd4b40b8c
Ruchit Modi <modi.ruchit6@gmail.com>
2019-11-23 01:19:44 -0500

    Added code to compute p_value using chisquared distribution

--- q1_classifier.py
+++ q1_classifier.py
@@ -1,14 +1,18 @@
-from scipy.stats import chisquare
 import argparse
+import os.path
+
 import numpy as np
 import pandas as pd
+from scipy.stats import chi2
 
 
 def train_test_split(train_file, test_file, y_train_file, y_test_file):
     X_train = pd.read_csv(train_file, header=None, sep='\s+')
     X_test = pd.read_csv(test_file, header=None, sep='\s+')
     y_train = pd.read_csv(y_train_file, header=None, sep='\s+')[0]
-    y_test = pd.read_csv(y_test_file, header=None, sep='\s+')[0]
+    y_test = None
+    if os.path.isfile(y_test_file):
+        y_test = pd.read_csv(y_test_file, header=None, sep='\s+')[0]
     return X_train, X_test, y_train, y_test
 
 
@@ -32,7 +36,7 @@ class ID3DecisionTree:
             curr_node = self.create_max_label_node(y)
         else:
             attr_to_split = self.least_entropy_attr(X, y)
-            p_value = self.chisquare_split(X, y)
+            p_value = self.compute_chisquare(X[attr_to_split], y)
 
             if p_value < self.p_value_threshold:
                 curr_node = TreeNode(val=attr_to_split + 1)
@@ -43,7 +47,7 @@ class ID3DecisionTree:
                         curr_node = self.create_max_label_node(y)
                     else:
                         new_X.drop(attr_to_split, axis=1, inplace=True)
-                        new_y = y.iloc[new_X.index]
+                        new_y = y.loc[new_X.index]
                         self.fit(new_X, new_y, parent=curr_node)
             else:
                 curr_node = self.create_max_label_node(y)
@@ -75,7 +79,7 @@ class ID3DecisionTree:
         for category in attr_values.unique():
             category_values = attr_values[attr_values == category]
             category_count = float(len(category_values))
-            category_target_values = target_values.iloc[category_values.index]
+            category_target_values = target_values.loc[category_values.index]
             y_counts = category_target_values.value_counts().to_dict()
             if len(y_counts) != 2:
                 e = 0
@@ -87,8 +91,32 @@ class ID3DecisionTree:
         return entropy
 
     @staticmethod
-    def chisquare_split(X, y):
-        return 1
+    def compute_chisquare(attr_values, target_values):
+        f_obs, f_exp = [], []
+        p, n = 0, 0
+        target_value_counts = target_values.value_counts().to_dict()
+        if 1 in target_value_counts:
+            p = target_value_counts[1]
+        if 0 in target_value_counts:
+            n = target_value_counts[0]
+        N = float(len(attr_values))
+        S = 0.0
+        for category in attr_values.unique():
+            category_values = attr_values[attr_values == category]
+            T_i = float(len(category_values))
+            category_target_values = target_values.loc[category_values.index]
+            y_counts = category_target_values.value_counts().to_dict()
+            p_i, n_i = 0, 0
+            if 1 in y_counts:
+                p_i = y_counts[1]
+            if 0 in y_counts:
+                n_i = y_counts[0]
+            p_prime_i = p * T_i / N
+            n_prime_i = n * T_i / N
+            S += (((p_prime_i - p_i) ** 2) / p_prime_i) + (((n_prime_i - n_i) ** 2) / n_prime_i)
+        m = len(attr_values.unique())
+        p_value = 1 - chi2.cdf(x=S, df=m - 1)
+        return p_value
 
     def predict(self, X):
         pass
@@ -98,7 +126,7 @@ class ID3DecisionTree:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-p', dest='p_value_threshold', action='store', type=float, help='pvalue')
+    parser.add_argument('-p', dest='p_value_threshold', action='store', type=float, help='pvalue threshold')
     parser.add_argument('-f1', dest='train_dataset', action='store', type=str, help='train_dataset')
     parser.add_argument('-f2', dest='test_dataset', action='store', type=str, help='test_dataset')
     parser.add_argument('-o', dest='output_file', action='store', type=str, help='output_file')
